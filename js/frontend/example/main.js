95a606e1113d83c1cbeb60f52eeeea1b363a2da9
Nick <nschaubeck@gmail.com>
2015-03-28 17:25:49 -0400

    Implemented catmull rom spline for curves

--- js/frontend/example/main.js
+++ js/frontend/example/main.js
@@ -2,6 +2,7 @@ var glShader = require('gl-shader')
 var glslify = require("glslify");
 var glMatrix = require("gl-matrix");
 var mat4 = glMatrix.mat4;
+var vec2 = glMatrix.vec2;
 
 function initWebGL(canvas) {
   gl = null;
@@ -23,130 +24,122 @@ function initWebGL(canvas) {
 
 window.canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
+var width = 600;
+var height = 600;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
 
-var gl = initWebGL(canvas);
-gl.clearColor(0.0, 1.0, 1.0, 1.0);
+var p1 = [0, height];
+var p2 = [width, 0];
 
-function startProgram(event) {
-  var image = event.target;
-  var width = image.width;
-  var height = image.height;
-  canvas.width = width;
-  canvas.height = height;
-  gl.viewport(0, 0, width, height);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+var slope = (p2[0] - p1[0]) / (p2[1] - p1[1]);
 
-  var img = event.currentTarget;
-  var shader = glShader(gl,
-    glslify('../../../src/shaders/texture_coords.vert'),
-    glslify('../../../src/shaders/levels.frag')
-  );
-  shader.bind();
+var p0 = [p1[0] - 10, p1[1] - (10 * 1/slope)];
+var p3 = [p2[0] + 10, p2[1] + (10 * 1/slope)];
 
-  //Create vertex buffer
-  var buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-     1.0,  1.0,  0.0,
-     0.0,  1.0,  0.0,
-     1.0,  0.0,  0.0,
-     0.0,  0.0,  0.0
-  ]), gl.STATIC_DRAW);
+var points = [p0, p1, p2, p3];
 
-  // Create texture coord buffer
-  // var texBuffer = gl.createBuffer();
-  // gl.bindBuffer(gl.ARRAY_BUFFER, texBuffer);
-  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+var newPoint = vec2.create();
 
-  //Set attributes
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  shader.attributes.position.pointer();
-
-  // Set uniforms
-  var orthoMat = mat4.ortho([], 0, width, 0, height, 0, 1);
-  var mvMatrix = mat4.scale([], mat4.create(), [width, height, 0]);
-
-  // Setup texture
-  var texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-
-  // Bind texture
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-
-  shader.uniforms.texture = 0;
-  shader.uniforms.p_matrix = orthoMat;
-  shader.uniforms.mv_matrix = mvMatrix;
-  shader.uniforms.rgb_in_min = 0.0;
-  shader.uniforms.rgb_in_max = 1.0;
-  shader.uniforms.rgb_out_min = 0.0;
-  shader.uniforms.rgb_out_max = 1.0;
-  shader.uniforms.rgb_gamma = 1.0;
-
-  shader.uniforms.r_in_min = 0.0;
-  shader.uniforms.r_in_max = 1.0;
-  shader.uniforms.r_out_min = 0.0;
-  shader.uniforms.r_out_max = 1.0;
-  shader.uniforms.r_gamma = 1.0;
-
-  shader.uniforms.g_in_min = 0.0;
-  shader.uniforms.g_in_max = 1.0;
-  shader.uniforms.g_out_min = 0.0;
-  shader.uniforms.g_out_max = 1.0;
-  shader.uniforms.g_gamma = 1.0;
-
-  shader.uniforms.b_in_min = 0.0;
-  shader.uniforms.b_in_max = 1.0;
-  shader.uniforms.b_out_min = 0.0;
-  shader.uniforms.b_out_max = 1.0;
-  shader.uniforms.b_gamma = 1.0;
-
-  var $control = $("#control");
-  var $control2 = $("#control2");
-  var $control3 = $("#control3");
-  var $control4 = $("#control4");
-  var $control5 = $("#control5");
-
-  $control.on('mousemove', function(event) {
-    var num = event.target.valueAsNumber / 100;
-    shader.uniforms.g_in_min = num;
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+canvas.onmousedown = function(event) {
+  newPoint = [event.layerX, event.layerY];
+  points.push(newPoint);
+  points.sort(function(a, b) {
+    return a[0] - b[0];
   });
-  $control2.on('mousemove', function(event) {
-    var num = event.target.valueAsNumber / 100;
-    shader.uniforms.g_in_max = num;
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  });
-  $control3.on('mousemove', function(event) {
-    var num = event.target.valueAsNumber / 100;
-    shader.uniforms.g_out_min = num;
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  });
-  $control4.on('mousemove', function(event) {
-    var num = event.target.valueAsNumber / 100;
-    shader.uniforms.g_out_max = num;
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  });
-  $control5.on('mousemove', function(event) {
-    var num = event.target.valueAsNumber / 100;
-    shader.uniforms.g_gamma = num;
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
+
+canvas.onmousemove = function(event) {
+  if (newPoint) {
+    newPoint[0] = event.layerX;
+    newPoint[1] = event.layerY;
+    points.sort(function(a, b) {
+      return a[0] - b[0];
+    });
+  }
+};
+
+canvas.onmouseup = function(event) {
+  newPoint = false;
+};
+
+var interpolatePoint = function(p0, p1, p2, p3, t0, t1, t2, t3, t) {
+  var a1, a2, a3, b1, b2, c;
+  a1 = [];
+  a2 = [];
+  a3 = [];
+  b1 = [];
+  b2 = [];
+  c = [];
+
+  vec2.add(a1, vec2.scale([], p0, (t1 - t)/(t1 - t0)), vec2.scale([], p1, (t - t0)/(t1 - t0)));
+  vec2.add(a2, vec2.scale([], p1, (t2 - t)/(t2 - t1)), vec2.scale([], p2, (t - t1)/(t2 - t1)));
+  vec2.add(a3, vec2.scale([], p2, (t3 - t)/(t3 - t2)), vec2.scale([], p3, (t - t2)/(t3 - t2)));
+
+  vec2.add(b1, vec2.scale([], a1, (t2 - t)/(t2 - t0)), vec2.scale([], a2, (t - t0)/(t2 - t0)));
+  vec2.add(b2, vec2.scale([], a2, (t3 - t)/(t3 - t1)), vec2.scale([], a3, (t - t1)/(t3 - t1)));
+
+  vec2.add(c, vec2.scale([], b1, (t2 - t)/(t2 - t1)), vec2.scale([], b2, (t - t1)/(t2 - t1)));
+
+  return c;
+};
+
+var catmullRomSplineSegment = function(p0, p1, p2, p3, samples, knot) {
+  var t, t0, t1, t2, t3;
+  knot = knot || 0.5
+  var points = [];
+
+  t0 = 0;
+  t1 = Math.pow((Math.sqrt(Math.pow((p1[0] - p0[0]), 2) + Math.pow((p1[1] - p0[1]), 2))), knot);
+  t2 = Math.pow((Math.sqrt(Math.pow((p2[0] - p1[0]), 2) + Math.pow((p2[1] - p1[1]), 2))), knot) + t1;
+  t3 = Math.pow((Math.sqrt(Math.pow((p3[0] - p2[0]), 2) + Math.pow((p3[1] - p2[1]), 2))), knot) + t2;
+
+  var sampleStep = (t2 - t1) / samples;
+  var t = t1;
+  while (t < t2) {
+    t += sampleStep;
+    points.push(interpolatePoint(p0, p1, p2, p3, t0, t1, t2, t3, t));
+  }
+
+  return points;
+};
+
+// Return a fully interpolated spline through all the control points.
+// Control points are included in the
+var catmullRomSpline = function(controlPoints, samples, knot) {
+  if (controlPoints.length < 4) {
+    throw "Must have at least 4 control points to generate catmull rom spline";
+  }
+  var points = [];
+  var p0, p1, p2, p3;
+
+  controlPoints.forEach(function(point, i) {
+    p0 = point;
+    p1 = controlPoints[i + 1];
+    p2 = controlPoints[i + 2];
+    p3 = controlPoints[i + 3];
+
+    if (!(p1 && p2 && p3)) {
+      return;
+    }
+
+    points.push(p1);
+    points = points.concat(catmullRomSplineSegment(p0, p1, p2, p3, samples, knot));
   });
 
+  return points;
+};
 
-  //Draw
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-}
+var frameLoop = function() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, width, height);
 
-var image = new Image();
-image.onload = startProgram;
-image.src = '../assets/chimp.jpg'
-document.body.appendChild(image);
+  catmullRomSpline(points, 1000, 0.5).forEach(function(point, index, array) {
+    context.fillStyle = "white";
+    context.fillRect(point[0], point[1], 1, 1);
+  });
+  requestAnimationFrame(frameLoop);
+};
 
-window.gl = gl;
+requestAnimationFrame(frameLoop);
