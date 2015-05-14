1947e401ced3933b25ade22e72c0066a7ad1b05e
Nick <nschaubeck@gmail.com>
2015-05-14 18:44:32 -0400

    Layout for demo setup, thank you @adamchristie

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,100 +2,53 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    express: {
-      web: {
-        options: {
-          script: 'js/backend/app.js',
-        }
-      },
-    },
     browserify: {
-      example_watch: {
-        src: ['js/frontend/example/main.js'],
-        dest: 'js/builds/example.js',
+      demo_watch: {
+        src: ['demo/main.js'],
+        dest: 'demo/build.js',
         options: {
           transform: ['glslify'],
           watch: true,
           keepAlive: true
         }
       },
-      example: {
-        src: ['js/frontend/example/main.js'],
-        dest: 'js/builds/example.js',
+      demo: {
+        src: ['demo/main.js'],
+        dest: 'demo/build.js',
         options: {
           transform: ['glslify']
         }
       }
     },
-
     watch: {
-      frontend: {
-        options: {
-          livereload: true
-        },
-        files: [
-          'views/**/*.html',
-          'js/builds/*.js'
-        ]
-      },
-      example: {
-        files: [
-          'js/frontend/example/**/*.js'
-        ],
-        tasks: [
-          'browserify:example'
-        ]
-      },
       shaders: {
         files: [
           'shaders/**/*'
         ],
         tasks: [
-          'browserify:example'
+          'browserify:demo'
         ]
-      },
-      web: {
-        files: [
-          'js/backend/**/*.js'
-        ],
-        tasks: [
-          'express:web'
-        ],
-        options: {
-          nospawn: true, //Without this option specified express won't be reloaded
-          atBegin: true,
-        }
       }
     },
     parallel: {
-      web: {
+      demo: {
         options: {
           stream: true
         },
         tasks: [
           {
             grunt: true,
-            args: ['watch:frontend']
-          },
-          {
-            grunt: true,
             args: ['watch:shaders']
           },
           {
             grunt: true,
-            args: ['browserify:example_watch']
-          },
-          {
-            grunt: true,
-            args: ['watch:web']
+            args: ['browserify:demo_watch']
           }
         ]
       },
     }
   });
-  grunt.registerTask('web', 'launch webserver and watch tasks', [
-    'parallel:web',
+  grunt.registerTask('demo_watch', 'launch webserver and watch tasks', [
+    'parallel:demo',
   ]);
-
-  grunt.registerTask('default', ['web']);
 };
